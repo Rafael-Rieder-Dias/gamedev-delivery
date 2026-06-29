88246a55198e5f1d41f14e22d3d8f19c8e1afc39
Rafael-Rieder-Dias <rafael.rieder@edu.pucrs.br>
2026-06-29 18:57:40 -0300

    Adição de uma recuperção de energia após um parry

--- scenes/fase_2.gd
+++ scenes/fase_2.gd
@@ -9,14 +9,18 @@ extends Node2D
 @onready var vitoria_area = $Vitoria/Vitoria_Area
 @onready var label_vitoria = $Controle/Control/LabelVitoria
 @onready var fade = $Controle/Control/Fade
+@onready var tutorial_panel = $Controle/Control/Tutorial/Tutorial_Panel
+@onready var tutorial_text = $Controle/Control/Tutorial/Tutorial_Panel/Tutorial_Text
+@onready var tilemaps = $TileMaps
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#world_boundary.body_entered.connect(_on_world_boundary_body_entered)
-	#player.PlayerDeath.connect(_on_player_death)
+	world_boundary.body_entered.connect(_on_world_boundary_body_entered)
+	player.PlayerDeath.connect(_on_player_death)
 	vitoria_area.body_entered.connect(_on_vitoria_area_body_entered)
 	label_vitoria.visible = false
 	fade.visible = false
+	tutorial_panel.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,3 +37,36 @@ func _on_vitoria_area_body_entered(body: Node) -> void:
 	label_vitoria.visible = true
 	await get_tree().create_timer(2.0).timeout
 	get_tree().change_scene_to_file("res://scenes/fase_2.tscn")
+	
+func _on_world_boundary_body_entered(body: Node) -> void:
+	if body != player:
+		return
+	player._on_hitkill()
+
+func _on_player_death() -> void:
+	if player_camera != null and death_camera != null:
+		death_camera.global_position = player_camera.global_position
+		death_camera.offset = player_camera.offset
+		death_camera.rotation = player_camera.rotation
+		death_camera.zoom = player_camera.zoom
+		player_camera.enabled = false
+		death_camera.enabled = true
+	musica.stream = death_sfx
+	musica.play()
+	await get_tree().create_timer(2).timeout
+	get_tree().reload_current_scene()
+
+func _on_placa_entered(body: Node, placa: Area2D) -> void:
+	if body != player:
+		return
+	
+	tutorial_panel.visible = true
+
+	match placa.name:
+		"Placa_Perigo":
+			tutorial_text.text = "Pulo frontal de fé à frente!"
+			
+func _on_placa_exited(body: Node, placa: Area2D) -> void:
+	if body != player:
+		return
+	tutorial_panel.visible = false
